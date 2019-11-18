07c74b524922379325551cb86e97d1b25a4d71f3
syntaxaire <syntaxaire@gmail.com>
2019-11-18 14:52:01 -0500

    Fix lint errors for CI

--- cogs/cryochamber.py
+++ cogs/cryochamber.py
@@ -1,7 +1,8 @@
 import re
 
-from discord import Embed, Attachment, Colour
+from discord import Embed, Colour
 from discord.ext.commands import Cog, CommandError, MessageConverter, TextChannelConverter, group
+
 
 class Cryochamber(Cog):
     def __init__(self, _):
@@ -11,32 +12,38 @@ class Cryochamber(Cog):
     async def preserve(self, context, *, arg):
         get_channel = TextChannelConverter().convert
         get_message = MessageConverter().convert
-        command_match = re.fullmatch(r'(?P<source_specifier>.*?) in (?P<destination_channel_specifier>.*?)', arg)
+        exp = r'(?P<source_specifier>.*?) in (?P<destination_channel_specifier>.*?)'
+        command_match = re.fullmatch(exp, arg)
 
         if command_match is None:
             raise CommandError('wrong syntax: ' + arg)
 
-        destination_channel = await get_channel(context, command_match.group('destination_channel_specifier'))
+        dest_channel = await get_channel(context,
+                                         command_match.group('destination_channel_specifier'))
 
-        if not destination_channel.permissions_for(context.author).manage_messages:
+        if not dest_channel.permissions_for(context.author).manage_messages:
             raise MissingPermissions(['manage_messages'])
 
-        source_channel_specifier_match = re.fullmatch(r'(?:(?P<temporal_modifier>future|no more) )?pins from (?P<source_channel_specifier>.*?)', command_match.group('source_specifier'))
+        exp = r'(?:(?P<temporal_modifier>future|no more) )' \
+              r'?pins from (?P<source_channel_specifier>.*?)'
+        source_channel_match = re.fullmatch(exp,
+                                            command_match.group('source_specifier'))
 
-        if source_channel_specifier_match is None:
+        if source_channel_match is None:
             source_message = await get_message(context, command_match.group('source_specifier'))
-            await self._preserve_message(source_message, destination_channel)
+            await self._preserve_message(source_message, dest_channel)
         else:
-            source_channel = await get_channel(context, source_channel_specifier_match.group('source_channel_specifier'))
-            temporal_modifier = source_channel_specifier_match.group('temporal_modifier')
+            src_channel = await get_channel(context,
+                                            source_channel_match.group('source_channel_specifier'))
+            temporal_modifier = source_channel_match.group('temporal_modifier')
 
             if temporal_modifier is None:
-                for pin in sorted(await source_channel.pins(), key=lambda message: message.created_at):
-                    await self._preserve_message(pin, destination_channel)
+                for pin in sorted(await src_channel.pins(), key=lambda message: message.created_at):
+                    await self._preserve_message(pin, dest_channel)
             elif temporal_modifier == 'future':
-                self.ongoing_preservations.add((source_channel.name, destination_channel.name))
+                self.ongoing_preservations.add((src_channel.name, dest_channel.name))
             elif temporal_modifier == 'no more':
-                self.ongoing_preservations.remove((source_channel.name, destination_channel.name))
+                self.ongoing_preservations.remove((src_channel.name, dest_channel.name))
             else:
                 raise ValueError('unknown temporal modifier: ' + temporal_modifier)
 
@@ -47,13 +54,19 @@ class Cryochamber(Cog):
     async def _preserve_message(self, message, channel):
         attach_str = ""
         if len(message.attachments) > 0:
-            attach_str = "" + str(len(message.attachments)) + " attachment" + ("s" if len(message.attachments) > 1 else "") + ""
+            attach_str = "" + str(len(message.attachments)) + " attachment" +\
+                         ("s" if len(message.attachments) > 1 else "") + ""
 
-        embedded_msg = Embed(colour=Colour(0xf403f), description=message.content, timestamp=message.created_at)
-        embedded_msg.set_author(name=message.author.name + '#' + message.author.discriminator + ", aka " + message.author.display_name, icon_url= str(message.author.avatar_url))
-        embedded_msg.add_field(name="__              __", value=attach_str + " [(original)](" + message.jump_url + ")")
+        embedded_msg = Embed(colour=Colour(0xf403f),
+                             description=message.content,
+                             timestamp=message.created_at)
+        embedded_msg.set_author(name=message.author.name + '#' + message.author.discriminator +
+                                ", aka " + message.author.display_name,
+                                icon_url=str(message.author.avatar_url))
+        embedded_msg.add_field(name="__              __",
+                               value=attach_str + " [(original)](" + message.jump_url + ")")
         embedded_msg.set_footer(text="in #" + message.channel.name)
         for attach in message.attachments:
             if attach.width is not None and attach.height is not None:
-                embedded_msg.set_image(url = attach.url)
+                embedded_msg.set_image(url=attach.url)
         await channel.send(embed=embedded_msg)
