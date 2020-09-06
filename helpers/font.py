5891c77ce54ec3612b5d2e48a16e38f5aa49b215
syntaxaire <syntaxaire@gmail.com>
2020-09-06 17:40:12 -0400

    Add support for Qud color templates to say command.

--- helpers/font.py
+++ helpers/font.py
@@ -1,7 +1,11 @@
 ﻿from textwrap import TextWrapper
 
 from hagadias import constants
+from hagadias.helpers import iter_qud_colors, strip_newstyle_qud_colors
 from PIL import Image, ImageFont, ImageDraw
+
+from shared import gameroot
+game_colors = gameroot.get_colors()
 
 
 QUD_WHITE = constants.QUD_COLORS['y']
@@ -24,45 +28,67 @@ class DrawException(Exception):
         self.message = message
 
 
-def drawttf(saying, bordertype='-popupclassic', arg='') -> Image:
+def drawttf(saying, bordertype='-popupclassic', dialog_title='') -> Image:
     """Main function for drawing the message box."""
-    if arg is None:
-        arg = ''
+    if dialog_title is None:
+        dialog_title = ''
     if bordertype is None:
         bordertype = '-p'
-    # split text and wrap it
+    # first, wrap and split the plaintext, then reassemble with colours
+    plain_saying = strip_newstyle_qud_colors(saying)
     wrapper = TextWrapper(width=MAXW, max_lines=MAXH, replace_whitespace=False)
-    sayingarr = saying.split('\n')
-    a = []
-    for paragraph in sayingarr:
-        # split further using regex?
-        a.append(wrapper.fill(paragraph))
-    saying = '\n'.join(a)
-    sayingdim = FONT.getsize_multiline(saying, spacing=-1)
+    paragraphs = plain_saying.split('\n')
+    text_lines = []
+    for paragraph in paragraphs:
+        text_lines.extend(wrapper.fill(paragraph).split('\n'))
+    # we will use the plain text wrapped by TextWrapper to count real characters on each line,
+    # then draw those characters using the parsed-out per-character color codes:
+    plain_text_for_sizing = '\n'.join(text_lines)
+    sayingdim = FONT.getsize_multiline(plain_text_for_sizing, spacing=-1)
     # determine border type
-    kind = determineborder(bordertype)
-    # generate image dimensions
-    temptextw = sayingdim[0]
-    temptitlew = (FONT.getsize(f'[ {arg} ]')[0] if kind == DIALOGUECLASSIC
-                  else 0)
-    tempminw = MINW * CHARSIZE[0]
-    tempw = max(temptextw, temptitlew, tempminw)
-    imgdim = (tempw + (2 * ABSINNERPAD[0]), sayingdim[1] + (2 * ABSINNERPAD[1]))
-    # generate base viridian color.
+    border_kind = determineborder(bordertype)
+    # determine image dimensions
+    base_pxwidth = MINW * CHARSIZE[0]
+    text_pxwidth = sayingdim[0]
+    title_pxwidth = (FONT.getsize(f'[ {dialog_title} ]')[0] if
+                     border_kind == DIALOGUECLASSIC else 0)
+    pxwidth = max(base_pxwidth, text_pxwidth, title_pxwidth)
+    pad_pxwidth = pxwidth + (2 * ABSINNERPAD[0])
+    pad_pxheight = sayingdim[1] + (2 * ABSINNERPAD[1])
+    imgdim = (pad_pxwidth, pad_pxheight)
+    # create and draw image
     image = Image.new(mode='RGBA', size=imgdim, color=QUD_VIRIDIAN)
     draw = ImageDraw.Draw(image)
-    # draw border
-    drawborder(kind, draw, imgdim, PAD, CHARSIZE, arg)
-    # print text
-    if kind == POPUPCLASSIC:
-        temp = (imgdim[0] - temptextw) / 2  # center text if popup
+    drawborder(border_kind, draw, imgdim, PAD, CHARSIZE, dialog_title)
+    if border_kind == POPUPCLASSIC:
+        leftx = (imgdim[0] - text_pxwidth) / 2  # center text if popup
     else:
-        temp = (imgdim[0] - tempw) / 2
-    if temp < ABSINNERPAD[0]:
-        temp = ABSINNERPAD[0]
-    draw.multiline_text((temp, ABSINNERPAD[1] - 4), saying, font=FONT, spacing=-1,
-                        fill=QUD_WHITE)
-
+        leftx = (imgdim[0] - pxwidth) / 2
+    if leftx < ABSINNERPAD[0]:
+        leftx = ABSINNERPAD[0]
+    # draw.multiline_text((leftx, ABSINNERPAD[1] - 4), text, font=FONT, spacing=-1,
+    #                     fill=QUD_WHITE)
+    cur_x = leftx
+    cur_y = ABSINNERPAD[1] - 4
+    chars_colors = iter_qud_colors(saying, game_colors)
+    for line in text_lines:
+        print(line)
+        for tracking, (char, code) in zip(line, chars_colors):
+            # fast-forward if necessary to skip whitespace that was deleted by TextWrapper
+            while char != tracking:
+                try:
+                    char, code = next(chars_colors)
+                except StopIteration:
+                    break
+            if code is None:
+                color = constants.QUD_COLORS['y']
+            else:
+                color = constants.QUD_COLORS[code]
+            draw.text((cur_x, cur_y), char, font=FONT, fill=color)
+            cur_x += CHARSIZE[0]
+            print(tracking, char, color)
+        cur_x = leftx
+        cur_y += CHARSIZE[1]
     # image post processing
     image = drawscanline(image)
     return image
