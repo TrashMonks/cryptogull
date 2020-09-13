b745f9ae2ab4afdaa38fe3fc87403ab1de14d638
syntaxaire <syntaxaire@gmail.com>
2020-09-13 10:47:48 -0400

    Log exceptions instead of halting Reddit tasks
    Fix exception when self-posts have no thumbnail

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -11,6 +11,8 @@ log = logging.getLogger('bot.' + __name__)
 
 
 class Reddit(Cog):
+    """Functionality for streaming Reddit posts and comments from one or more
+    subreddits to one or more channels."""
     def __init__(self, bot: Bot):
         self.bot = bot
         self.config = config['Reddit']
@@ -21,6 +23,7 @@ class Reddit(Cog):
 
         @self.bot.event
         async def on_ready():
+            """Register all requested subreddit+channel combos as tasks."""
             self.loop = asyncio.get_running_loop()
             for subreddit_name, channels in self.config['subreddits'].items():
                 subreddit = await self.reddit.subreddit(subreddit_name)
@@ -35,33 +38,40 @@ class Reddit(Cog):
 async def relay_submissions(subreddit, channel):
     """Watch for new submissions in the subreddit and relay them in embeds."""
     async for submission in subreddit.stream.submissions(skip_existing=True):
-        await submission.load()
-        embed = Embed(title=submission.title,
-                      url=submission.url,
-                      description=submission.selftext)
-        # need to check 'vars' because submission cannot be directly queried, see:
-        # https://asyncpraw.readthedocs.io/en/latest/getting_started/quick_start.html#determine-available-attributes-of-an-object
-        if 'thumbnail' in vars(submission):
-            embed.set_thumbnail(url=submission.thumbnail)
-        author = submission.author
-        await author.load()
-        embed.set_author(name=author.name,
-                         url=f'https://reddit.com/u/{author.name}',
-                         icon_url=author.icon_img)
-        await channel.send(embed=embed)
+        try:
+            await submission.load()
+            embed = Embed(title=submission.title,
+                          url=submission.url,
+                          description=submission.selftext)
+            # need to check 'vars' because submission cannot be directly queried, see:
+            # https://asyncpraw.readthedocs.io/en/latest/getting_started/quick_start.html#determine-available-attributes-of-an-object
+            if 'thumbnail' in vars(submission) and submission.thumbnail != 'self':
+                # self-posts (text) have the text string 'self' instead of a thumbnail
+                embed.set_thumbnail(url=submission.thumbnail)
+            author = submission.author
+            await author.load()
+            embed.set_author(name=author.name,
+                             url=f'https://reddit.com/u/{author.name}',
+                             icon_url=author.icon_img)
+            await channel.send(embed=embed)
+        except Exception as e:
+            log.exception(e)
 
 
 async def relay_comments(subreddit, channel):
     """Watch for new comments in the subreddit and relay them in embeds."""
     async for comment in subreddit.stream.comments(skip_existing=True):
-        permalink = f'https://reddit.com{comment.permalink}'
-        description = f'**[New reply]({permalink}) on ' \
-                      f'[{comment.link_title}]({comment.link_permalink})**\n'
-        description += comment.body[:1500]
-        embed = Embed(description=description)
-        author = comment.author
-        await author.load()
-        embed.set_author(name=author.name,
-                         url=f'https://reddit.com/u/{author.name}',
-                         icon_url=author.icon_img)
-        await channel.send(embed=embed)
+        try:
+            permalink = f'https://reddit.com{comment.permalink}'
+            description = f'**[New reply]({permalink}) on ' \
+                          f'[{comment.link_title}]({comment.link_permalink})**\n'
+            description += comment.body[:1500]
+            embed = Embed(description=description)
+            author = comment.author
+            await author.load()
+            embed.set_author(name=author.name,
+                             url=f'https://reddit.com/u/{author.name}',
+                             icon_url=author.icon_img)
+            await channel.send(embed=embed)
+        except Exception as e:
+            log.exception(e)
