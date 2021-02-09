0d4d214741daf60d1947b61c1c3e45ab6fbde9f2
Dij <racheltao@outlook.com>
2021-02-09 17:02:19 -0500

    add qud version

--- cogs/hitdabricks.py
+++ cogs/hitdabricks.py
@@ -14,10 +14,16 @@ class Hitdabricks(Cog):
     async def hitdabricks(self, ctx: Context, *args):
         """Send golden advice."""
         log.info(f'({ctx.message.channel}) <{ctx.message.author}> hit da bricks!')
-        embed = Embed(colour=Colour(0xf403f),
-                      url="https://twitter.com/dasharez0ne/status/979810839749210112",
-                      description="(source: [dasharez0ne](https://twitter.com/dasharez0ne/"
-                      + "status/979810839749210112))")
-
-        embed.set_image(url="https://pbs.twimg.com/media/DZj8pB8VMAAxPNR?format=jpg&name=small")
+        if ctx.message.content[12:].strip() == "but qud":
+            embed = Embed(colour=Colour(0xf403f),
+                          url="https://twitter.com/dasharez0ne/status/979810839749210112",
+                          description="(source: [eva problems](https://twitter.com/ohnoproblems/"
+                          + "status/1285818414754230274))")
+            embed.set_image(url="https://pbs.twimg.com/media/Edgkx-RU4AYxMpL?format=png&name=small")# noqa E722
+        else:
+            embed = Embed(colour=Colour(0xf403f),
+                          url="https://twitter.com/dasharez0ne/status/979810839749210112",
+                          description="(source: [dasharez0ne](https://twitter.com/dasharez0ne/"
+                          + "status/979810839749210112))")
+            embed.set_image(url="https://pbs.twimg.com/media/DZj8pB8VMAAxPNR?format=jpg&name=small")
         return await ctx.send(embed=embed)
