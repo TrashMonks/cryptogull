f442ce6e3c8e1e5dd42444dee6c04d9a3c0a348e
syntaxaire <syntaxaire@gmail.com>
2022-11-13 19:07:34 -0500

    Refactor Reddit cog
    This fixes a longstanding issue where new tasks would be created
    after a disconnect (where on_ready is called more than once).

--- bot/cogs/reddit.py
+++ bot/cogs/reddit.py
@@ -1,9 +1,10 @@
+"""Cog for posting Reddit events to Discord."""
 import asyncio
 import logging
 
+import asyncpraw
 from discord import Embed
 from discord.ext.commands import Cog, Bot
-import asyncpraw
 
 from bot.shared import config
 
@@ -20,64 +21,79 @@ class Reddit(Cog):
                                        client_secret=self.config['secret'],
                                        user_agent=self.config['user agent'])
         self.loop = None
+        self.tasks_created = False
 
         @self.bot.event
         async def on_ready():
             """Register all requested subreddit+channel combos as tasks."""
+            # guard against creating tasks more than once - on_ready fires when reconnecting.
+            if self.tasks_created:
+                return
             self.loop = asyncio.get_running_loop()
             for subreddit_name, channels in self.config['subreddits'].items():
                 subreddit = await self.reddit.subreddit(subreddit_name)
                 if 'submissions channel' in channels:
                     posts_channel = bot.get_channel(int(channels['submissions channel']))
-                    self.loop.create_task(relay_submissions(subreddit, posts_channel))
+                    self.loop.create_task(watch_submissions(subreddit, posts_channel))
                 if 'comments channel' in channels:
                     comments_channel = bot.get_channel(int(channels['comments channel']))
-                    self.loop.create_task(relay_comments(subreddit, comments_channel))
+                    self.loop.create_task(watch_comments(subreddit, comments_channel))
+            self.tasks_created = True
 
 
-async def relay_submissions(subreddit, channel):
-    """Watch for new submissions in the subreddit and relay them in embeds."""
+async def watch_submissions(subreddit, channel):
+    """Watch for new submissions in the subreddit and relay them to a processor."""
     while True:
         try:
             async for submission in subreddit.stream.submissions(skip_existing=True):
-                await submission.load()
-                embed = Embed(title=submission.title[:200],
-                              url=submission.url,
-                              description=submission.selftext[:1000])
-                # need to check 'vars' because submission cannot be directly queried, see:
-                # https://asyncpraw.readthedocs.io/en/latest/getting_started/quick_start.html#determine-available-attributes-of-an-object
-                if 'thumbnail' in vars(submission) and submission.thumbnail != 'self':
-                    # self-posts (text) have the text string 'self' instead of a thumbnail
-                    embed.set_thumbnail(url=submission.thumbnail)
-                author = submission.author
-                await author.load()
-                embed.set_author(name=author.name,
-                                 url=f'https://reddit.com/u/{author.name}',
-                                 icon_url=author.icon_img)
-                await channel.send(embed=embed)
+                await process_submission(channel, submission)
         except Exception as e:
             log.exception(e)
             log.warning('Caught exception in relay_submissions, continuing in 10 seconds.')
             await asyncio.sleep(10)
 
 
-async def relay_comments(subreddit, channel):
-    """Watch for new comments in the subreddit and relay them in embeds."""
+async def process_submission(channel, submission):
+    """Handle a submission to a subreddit and post it as an embed."""
+    await submission.load()
+    embed = Embed(title=submission.title[:200],
+                  url=submission.url,
+                  description=submission.selftext[:1000])
+    # need to check 'vars' because submission cannot be directly queried, see:
+    # https://asyncpraw.readthedocs.io/en/latest/getting_started/quick_start.html#determine-available-attributes-of-an-object
+    if 'thumbnail' in vars(submission) and submission.thumbnail != 'self':
+        # self-posts (text) have the text string 'self' instead of a thumbnail
+        embed.set_thumbnail(url=submission.thumbnail)
+    author = submission.author
+    await author.load()
+    embed.set_author(name=author.name,
+                     url=f'https://reddit.com/u/{author.name}',
+                     icon_url=author.icon_img)
+    await channel.send(embed=embed)
+
+
+async def watch_comments(subreddit, channel):
+    """Watch for new comments in the subreddit and relay them to a processor."""
     while True:
         try:
             async for comment in subreddit.stream.comments(skip_existing=True):
-                permalink = f'https://reddit.com{comment.permalink}'
-                description = f'**[New reply]({permalink}) on ' \
-                              f'[{comment.link_title}]({comment.link_permalink})**\n'
-                description += comment.body[:1500]
-                embed = Embed(description=description)
-                author = comment.author
-                await author.load()
-                embed.set_author(name=author.name,
-                                 url=f'https://reddit.com/u/{author.name}',
-                                 icon_url=author.icon_img)
-                await channel.send(embed=embed)
+                await process_comment(channel, comment)
         except Exception as e:
             log.exception(e)
             log.warning('Caught exception in relay_comments, continuing in 10 seconds.')
             await asyncio.sleep(10)
+
+
+async def process_comment(channel, comment):
+    """Handle a new comment in the subreddit and post it as an embed."""
+    permalink = f'https://reddit.com{comment.permalink}'
+    description = f'**[New reply]({permalink}) on ' \
+                  f'[{comment.link_title}]({comment.link_permalink})**\n'
+    description += comment.body[:1500]
+    embed = Embed(description=description)
+    author = comment.author
+    await author.load()
+    embed.set_author(name=author.name,
+                     url=f'https://reddit.com/u/{author.name}',
+                     icon_url=author.icon_img)
+    await channel.send(embed=embed)
