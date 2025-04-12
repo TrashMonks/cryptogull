96d2511c73521339a1caf4a64b63b100bbb40433
Wreckstation <53310247+Wreckstation@users.noreply.github.com>
2025-04-12 15:26:52 -0400

    add test

--- tests/random_tile_test.py
+++ tests/random_tile_test.py
@@ -17,3 +17,14 @@ async def test_random_tile():
     for i in range(100):
         name, args = get_random_tile_name()
         msg, filedata, filename = await get_tile_data(name, *args)
+
+@pytest.mark.skipif(CI, reason="Skipping due to no textures")
+@pytest.mark.asyncio
+async def test_random_generated_tile():
+    """Calls a random recolor of an object that has its tile generated
+    based on its glyphs like vortexes or gasses.
+
+    Skipped in integration tests due to textures not being present."""
+
+    msg, filedata, filename = await get_tile_data('GlitterGas80', *args)
+    msg, filedata, filename = await get_tile_data("Space-Time Anomaly", *args)
